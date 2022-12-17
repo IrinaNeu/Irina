6b9350ddc2892c0302dd1058bf9881840ca24fe0
IrinaNeu <120553867+IrinaNeu@users.noreply.github.com>
2022-12-17 15:06:05 +0700

    ДЗ

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// Задача 2 
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32 (Console.ReadLine());
+
+// Console.WriteLine("Введите число");
+// int number2=Convert.ToInt32 (Console.ReadLine());
+
+// if(number1>number2)
+// {
+//  Console.WriteLine(number1);
+// }
+// else 
+// {
+//     Console.WriteLine(number2);
+// }
+
+//_____________________________________________________________
+
+// Задача 4
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32 (Console.ReadLine());
+// 
+// Console.WriteLine("Введите число");
+// int number2=Convert.ToInt32 (Console.ReadLine());
+//
+// Console.WriteLine("Введите число");
+// int number3=Convert.ToInt32 (Console.ReadLine());
+//
+// int max = number1;
+// 
+// if(number2>max)
+// {
+//   max = number2;
+//    
+// }
+//  if(number3>max)
+//    max = number3;
+// 
+// {
+//     Console.WriteLine(max);
+// }
+//__________________________________________________________________________
+
+// Задача 6
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32 (Console.ReadLine());
+//
+// if(number1%2 == 0)
+// {
+//        Console.WriteLine("Четное число");
+// }        
+// else 
+// {
+//    Console.WriteLine("Нечетное число");
+// }       
+
+//_______________________________________________________
+
+
+// Задача 8
+
+// Console.WriteLine("Введите число");
+// int number1=Convert.ToInt32 (Console.ReadLine());
+// 
+// int numberNeg = 1;
+// 
+// if (numberNeg == 1); 
+// {
+//    numberNeg+= 1;
+// }
+// while (numberNeg <= number1)
+// {
+//     Console.WriteLine(numberNeg);   
+//     numberNeg+= 2;
+//   }
+
+
+
+
